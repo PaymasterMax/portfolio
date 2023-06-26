2e4229442d03f6e905b2696c3de784f53cf3d9d7
Morph <duncansantiago18@gmail.com>
2023-06-26 16:23:11 -0700

    portfolio

--- components/common/Menu.tsx
+++ components/common/Menu.tsx
@@ -1,63 +1,72 @@
-import styles from "../shared-css/Layout.module.css";
+import styles from '../shared-css/Layout.module.css';
 
 const Menu = (props) => {
-  return (
-    <div
-      className={styles._menu}
-      onBlur={(e) => {
-        alert("Hello there");
-      }}
-    >
-      <div
-        className={styles._link}
-        onClick={(e) => {
-          document.querySelector("#_page1")?.scrollIntoView();
-          props.handleHumbuggerOpen();
-        }}
-      >
-        Home
-      </div>
+	return (
+		<div
+			className={styles._menu}
+			onBlur={(e) => {
+				alert('Hello there');
+			}}>
+			<div
+				className={styles._link}
+				onClick={(e) => {
+					const targetEle = document.querySelector('#_page1');
+					props.handleHumbuggerOpen();
+					targetEle.scrollIntoView(false);
+				}}>
+				Home
+			</div>
 
-      <div
-        className={styles._link}
-        onClick={(e) => {
-          document.querySelector("#_page2")?.scrollIntoView();
-          props.handleHumbuggerOpen();
-        }}
-      >
-        Experience
-      </div>
+			<div
+				className={styles._link}
+				onClick={(e) => {
+					const targetEle = document.querySelector('#_page2');
+					props.handleHumbuggerOpen();
+					targetEle.scrollIntoView(false);
+				}}>
+				Experience
+			</div>
 
-      <div
-        className={styles._link}
-        onClick={(e) => {
-          document.querySelector("#_page3")?.scrollIntoView();
-          props.handleHumbuggerOpen();
-        }}
-      >
-        Skills
-      </div>
+			<div
+				className={styles._link}
+				onClick={(e) => {
+					const targetEle = document.querySelector('#_page3');
+					props.handleHumbuggerOpen();
+					targetEle.scrollIntoView(false);
+				}}>
+				Skills
+			</div>
 
-      <div
-        className={styles._link}
-        onClick={(e) => {
-          document.querySelector("#_page4")?.scrollIntoView();
-          props.handleHumbuggerOpen();
-        }}
-      >
-        Connect
-      </div>
-      <div
-        className={styles._link}
-        onClick={(e) => {
-          document.querySelector("#_page5")?.scrollIntoView();
-          props.handleHumbuggerOpen();
-        }}
-      >
-        Hobbies
-      </div>
-    </div>
-  );
+			<div
+				className={styles._link}
+				onClick={(e) => {
+					const targetEle = document.querySelector('#_page4');
+					props.handleHumbuggerOpen();
+					targetEle.scrollIntoView(false);
+				}}>
+				Connect
+			</div>
+			<div
+				className={styles._link}
+				onClick={(e) => {
+					const targetEle = document.querySelector('#_page5');
+					props.handleHumbuggerOpen();
+					targetEle.scrollIntoView(false);
+				}}>
+				Hobbies
+			</div>
+
+			<div
+				className={styles._link}
+				onClick={(e) => {
+					const targetEle = document.querySelector('#_page5');
+					props.handleHumbuggerOpen();
+					targetEle.scrollIntoView(false);
+				}}>
+				Play A TicTacToe
+			</div>
+		</div>
+	);
 };
 
 export default Menu;
